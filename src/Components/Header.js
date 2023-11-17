101f474e5d54f79d6a714775357963241527bc74
pradeep <pradeepkumar935373@gmail.com>
2023-11-17 12:40:37 +0530

    five

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 const Header = () => {
   return (
     <div>
-      <header className='header'>WELCOME TO MY WEBPAGE</header>
+      <header className='header'><marquee>WELCOME TO MY WEBPAGE</marquee></header>
       </div>
   )
 }
